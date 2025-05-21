848effb87efdbd6dd8657c6927cc16b9d0e34217
John <psg6868@yahoo.com>
2025-05-21 10:49:17 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import Image from 'next/image';
-import { Button } from '@/components/ui/button';
+
 import { Card, CardContent } from '@/components/ui/card';
 import { Download } from 'lucide-react';
 
